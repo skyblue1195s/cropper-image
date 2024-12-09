ff41cc2f7bedff1a68d01f2313b35f58fe11f3c7
Dieu Tran <loveblue1195@gmail.com>
2024-12-09 20:27:53 +0700

    init source

---
+++ src/app/app.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import { ImageCroppedEvent, ImageTransform, LoadedImage } from 'ngx-image-cropper';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'my-angular-app';
+  transform: ImageTransform = {
+    scale: 1,
+    rotate: 0,
+    flipH: false,
+    flipV: false,
+  };
+  imageChangedEvent: any = '';
+    croppedImage: any = '';
+
+    fileChangeEvent(event: any): void {
+        this.imageChangedEvent = event;
+    }
+    imageCropped(event: ImageCroppedEvent) {
+        this.croppedImage = event.base64;
+    }
+    imageLoaded(image: LoadedImage) {
+        // show cropper
+    }
+    cropperReady() {
+        // cropper ready
+    }
+    loadImageFailed() {
+        // show message
+    }
+}
