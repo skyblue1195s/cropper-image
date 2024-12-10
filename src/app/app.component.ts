145770da0a8c6780a4555108328b2fcdd18c7f4b
Dieu Tran <dieu@deltabrains.vn>
2024-12-10 17:44:47 +0700

    change lib cropper image

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,42 +1,107 @@
-import { Component } from '@angular/core';
-import { ImageCroppedEvent, ImageTransform, LoadedImage } from 'ngx-image-cropper';
-import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
+import { ChangeDetectionStrategy, Component, ViewChild } from '@angular/core';
+import { ThemeVariables, ThemeRef, lyl, StyleRenderer } from '@alyle/ui';
+import { Platform } from '@angular/cdk/platform';
+import {   STYLES as CROPPER_STYLES,
+  ImgCropperConfig, ImgCropperErrorEvent, ImgCropperEvent, ImgCropperLoaderConfig, LyImageCropper } from '@alyle/ui/image-cropper';
+import { LySliderChange } from '@alyle/ui/slider';
+import convert from 'heic-convert'
+import heic2any from 'heic2any';
+const STYLES = (theme: ThemeVariables, ref: ThemeRef) => {
+  ref.renderStyleSheet(CROPPER_STYLES);
+  const cropper = ref.selectorsOf(CROPPER_STYLES);  return {
+    root: lyl `{
+      ${cropper.root} {
+        max-width: 400px
+        height: 300px
+      }
+    }`,
+    sliderContainer: lyl `{
+      text-align: center
+      max-width: 400px
+      margin: 14px
+    }`,
+    cropResult: lyl `{
+      border-radius: 50%
+    }`
+  };
+};
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  providers: [StyleRenderer]
 })
 export class AppComponent {
-  title = 'my-angular-app';
-  transform: ImageTransform = {
-    scale: 1,
-    rotate: 0,
-    flipH: false,
-    flipV: false,
+  classes = this.sRenderer.renderSheet(STYLES, 'root');
+  croppedImage?: string;
+  scale: number;
+  ready: boolean;
+  minScale: number;
+  @ViewChild(LyImageCropper, { static: true }) readonly cropper: LyImageCropper;
+  myConfig: ImgCropperConfig = {
+    width: 150, // Default `250`
+    height: 150, // Default `200`
+    fill: '#ff2997', // Default transparent if type = png else #000
+    type: 'image/png', // Or you can also use `image/jpeg`
+    round: true
   };
-  imageChangedEvent: any = '';
-    croppedImage: SafeUrl = '';
 
-    constructor(
-      private sanitizer: DomSanitizer
-    ) {
-    }
-  
+  constructor(
+    readonly sRenderer: StyleRenderer,
+    private _platform: Platform
+  ) { }
+  ngAfterViewInit() {
 
-    fileChangeEvent(event: any): void {
-        this.imageChangedEvent = event;
+    // demo: Load image from URL and update position, scale, rotate
+    // this is supported only for browsers
+    if (this._platform.isBrowser) {
+      const config: ImgCropperLoaderConfig = {
+        scale: 0.745864772531767,
+        xOrigin: 642.380608078103,
+        yOrigin: 236.26357452128866,
+        // rotation: 90,
+        originalDataURL: 'https://firebasestorage.googleapis.com/v0/b/alyle-ui.appspot.com/o/img%2Flarm-rmah-47685-unsplash-1.png?alt=media&token=96a29be5-e3ef-4f71-8437-76ac8013372c'
+      };
+      this.cropper.loadImage(config);
     }
-    imageCropped(event: ImageCroppedEvent) {
-      this.croppedImage = this.sanitizer.bypassSecurityTrustUrl(event.base64 || '');
-    }
-    imageLoaded(image: LoadedImage) {
-        // show cropper
-    }
-    cropperReady() {
-        // cropper ready
-    }
-    loadImageFailed() {
-        // show message
-    }
+
+  }
+
+  onCropped(e: ImgCropperEvent) {
+    this.croppedImage = e.dataURL;
+    console.log('cropped img: ', e);
+  }
+  onLoaded(e: ImgCropperEvent) {
+    console.log('img loaded', e);
+  }
+  onError(e: ImgCropperErrorEvent) {
+    console.warn(`'${e.name}' is not a valid image`, e);
+  }
+  onSliderInput(event: LySliderChange) {
+    this.scale = event.value as number;
+  }
+
+  imageChange(event: any) {
+    const file = event.target.files[0];
+    this.fileReader(file);
+   
+    this.cropper.selectInputEvent(event)
+  }
+
+   fileReader(file: any) {
+    const reader = new FileReader();
+  reader.onload = async function(e) {
+      const blob = new Blob([new Uint8Array(file)], {type: file.type });
+      console.log(blob);
+      const data = await heic2any({
+        blob,
+        toType: "image/jpeg",
+        quality: 0.5, // cuts the quality and size by half
+      })
+      console.log(data);
+  };
+  reader.readAsArrayBuffer(file);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,16 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule, HAMMER_GESTURE_CONFIG, HammerModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ImageCropperModule } from 'ngx-image-cropper';
+import { NgxCropperJsModule } from 'ngx-cropperjs-wrapper';
+import { LyHammerGestureConfig, LyThemeModule, LY_THEME, LY_THEME_NAME, StyleRenderer, LyTheme2 } from '@alyle/ui';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MinimaLight } from '@alyle/ui/themes/minima';
+import { LyImageCropperModule } from '@alyle/ui/image-cropper';
+import { LySliderModule } from '@alyle/ui/slider';
+import { LyIconModule } from '@alyle/ui/icon';
 
 @NgModule({
   declarations: [
@@ -12,9 +19,14 @@ import { ImageCropperModule } from 'ngx-image-cropper';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ImageCropperModule
+    ImageCropperModule,
+    NgxCropperJsModule,
+    BrowserAnimationsModule,
+    LyImageCropperModule,
+    LyIconModule,
+    HammerModule
   ],
-  providers: [],
+  providers: [{ provide: HAMMER_GESTURE_CONFIG, useClass: LyHammerGestureConfig }, StyleRenderer, LyTheme2, { provide: LY_THEME_NAME, useValue: 'minima-light' }, { provide: LY_THEME, useClass: MinimaLight, multi: true }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
